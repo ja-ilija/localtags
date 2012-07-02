722a321402bba9a1d398e0369802cc424b7c322e
Srdjan Stipic <srdjan.stipic@gmail.com>
2012-07-02 15:57:37 +0200

    Implemented first version of tags.
    This plugin adds menu item 'Add to tag file'
    to the mouse right click.
    
    The plugin stores the url of the page and
    selected text to the file named 'tags.txt'.
    
    This version doesn't

--- helloworld/content/overlay.js
+++ helloworld/content/overlay.js
@@ -4,9 +4,44 @@ var HelloWorld = {
     this.initialized = true;
   },
 
-  onMenuItemCommand: function() {
-    window.open("chrome://helloworld/content/hello.xul", "", "chrome");
-  }
+  _getStream: function(file_name) {
+    Components.utils.import("resource://gre/modules/FileUtils.jsm");  
+  
+    // get the "data.txt" file in the profile directory  
+    var file = FileUtils.getFile("ProfD", [file_name]);
+
+    if (!file.exists()) {
+      file.create(Components.interfaces.nsIFile.NORMAL_FILE_TYPE, 0666);
+    }
+
+    var ostream = FileUtils.openFileOutputStream(file,
+                                                 FileUtils.MODE_WRONLY |
+                                                 FileUtils.MODE_CREATE |
+                                                 FileUtils.MODE_APPEND);
+
+    return ostream;
+  },
+
+  _textToStream: function(text) {
+    var converter = Components.classes["@mozilla.org/intl/scriptableunicodeconverter"].  
+                    createInstance(Components.interfaces.nsIScriptableUnicodeConverter);  
+    converter.charset = "UTF-8";  
+
+    var istream = converter.convertToInputStream(text);
+    return istream;
+  },
+
+  onItem1Command: function() {
+    var text = content.getSelection().toString();
+    text = JSON.stringify(text);
+    var url = window.top.getBrowser().selectedBrowser.contentWindow.location.href;
+
+    var ostream = HelloWorld._getStream("tags.txt");
+    var istream = HelloWorld._textToStream(url + " " + text + "\n");
+
+    NetUtil.asyncCopy(istream, ostream);
+  },
+
 };
 
 window.addEventListener("load", function(e) { HelloWorld.onLoad(e); }, false); 
